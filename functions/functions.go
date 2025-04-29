ae0b58f62228612816811d1304203c960bf3f2d6
giovvbatista <giovvbatista@gmail.com>
2025-04-29 20:54:46 -0300

    added more functions

--- functions/functions.go
+++ functions/functions.go
@@ -18,6 +18,14 @@ func ThirdFi(x float64) float64 {
 	return 6 / (x + 1)
 }
 
+func Fourth(x float64) float64 {
+	return 2*math.Pow(x, 4) + 4*math.Pow(x, 3) + 3*math.Pow(x, 2) - 10*x - 15
+}
+
+func SecondFourth(x float64) float64 {
+	return math.Pow(x, 5) - 2*math.Pow(x, 4) - 9*math.Pow(x, 3) + 22*math.Pow(x, 2) + 4*x - 24
+}
+
 func Derivative(x float64, function func(float64) float64) float64 {
 	eps := math.Pow(10, -8)
 	return (function(x+eps) - function(x)) / eps
